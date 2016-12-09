3c3fe857ec14e94e2614f0d27289eb337578952b
Ahmad Bawaneh <akabme@gmail.com>
2016-12-10 01:44:19 +0200

    fix imports and remove commented lines

--- SampleProject/rafa-app/rafa-app-server/src/main/java/org/akab/rafa/ServerAppListener.java
+++ SampleProject/rafa-app/rafa-app-server/src/main/java/org/akab/rafa/ServerAppListener.java
@@ -4,10 +4,8 @@ import org.akab.engine.core.server.ServerConfigurationLoader;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.servlet.annotation.WebListener;
 import java.util.logging.Logger;
 
-@WebListener
 public class ServerAppListener implements ServletContextListener {
 
     private static final Logger LOGGER= Logger.getLogger(ServerAppListener.class.getName());
--- SampleProject/rafa-app/rafa-app-server/src/main/java/org/akab/rafa/ServerServiceImpl.java
+++ SampleProject/rafa-app/rafa-app-server/src/main/java/org/akab/rafa/ServerServiceImpl.java
@@ -3,20 +3,12 @@ package org.akab.rafa;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import org.akab.engine.core.api.shared.request.ServerRequest;
 import org.akab.engine.core.api.shared.request.ServerResponse;
-import org.akab.engine.core.api.shared.server.HandlersRepository;
-import org.akab.engine.core.api.shared.server.InterceptorsRepository;
 import org.akab.engine.core.api.shared.server.ServerApp;
 import org.akab.engine.core.api.shared.service.ServerService;
-import org.akab.engine.core.server.InMemoryHandlersRepository;
-import org.akab.engine.core.server.InMemoryInterceptorsRepository;
 import org.akab.engine.core.server.RpcEntryPointContext;
-import org.akab.rafa.hello.world.server.HelloWorldHandler;
-import org.akab.engine.core.server.DefaultRequestExecutor;
 
-import javax.servlet.annotation.WebServlet;
 import java.util.logging.Logger;
 
-@WebServlet(value="/app/serverService" , name = "ServerRPC")
 public class ServerServiceImpl extends RemoteServiceServlet implements ServerService {
 
     private static final long serialVersionUID = 9016180344293259071L;
@@ -25,11 +17,6 @@ public class ServerServiceImpl extends RemoteServiceServlet implements ServerSer
 
     @Override
     public ServerResponse executeRequest(ServerRequest request) throws Exception {
-//        HandlersRepository handlersRepository=new InMemoryHandlersRepository();
-//        InterceptorsRepository interceptorsRepository=new InMemoryInterceptorsRepository();
-//        new ServerApp.ServerAppBuilder().handlersRepository(handlersRepository).interceptorsRepository(interceptorsRepository).executor(new DefaultRequestExecutor(handlersRepository, interceptorsRepository)).build();
-//        ServerApp sa=ServerApp.make();
-//        sa.registerHandler(HelloWordsArgs.class.getCanonicalName(), new HelloWorldHandler());
         return ServerApp.make().executeRequest(request, new RpcEntryPointContext(getThreadLocalRequest(), getThreadLocalResponse()));
     }
 }
